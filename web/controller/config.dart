ea5a829923190ffc5f3b51516775efdc783c613e
LordFuchur <s.moeller-neversdorf@web.de>
2016-04-19 18:48:08 +0200

    - add block settings to Config

--- web/controller/config.dart
+++ web/controller/config.dart
@@ -1,4 +1,17 @@
 class config
 {
+  static int dimBlockX = 4;
+  static int dimBlockY = 4;
 
+
+
+  static int getBlockSizeX()
+  {
+    return dimBlockX;
+  }
+
+  static int getBlockSizeY()
+  {
+    return dimBlockY;
+  }
 }

fd919b0083d334bcb201c03f2c2b0aa5566979b3
LordFuchur <s.moeller-neversdorf@web.de>
2016-04-19 19:06:34 +0200

    - add field settings to Config - cursor move method added

--- web/controller/config.dart
+++ web/controller/config.dart
@@ -2,9 +2,16 @@ class config
 {
   static int dimBlockX = 4;
   static int dimBlockY = 4;
+  static int fieldX = 24;
+  static int fieldY = 48;
 
+/* Field
+  upwards
+  [0][0] = column[0]row[0]
+  [0][1] = column[0]row[1]
+ */
 
-
+  // Block size getter
   static int getBlockSizeX()
   {
     return dimBlockX;
@@ -13,5 +20,16 @@ class config
   static int getBlockSizeY()
   {
     return dimBlockY;
+  }
+
+  // Field size getter
+  static int getFieldSizeX()
+  {
+    return fieldX;
+  }
+
+  static int getFieldSizeY()
+  {
+    return fieldY;
   }
 }

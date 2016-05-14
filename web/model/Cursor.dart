c6615d84fd1c45e7374c0bfa1367c03781c6a7fb
peremptor <peremptor1@gmx.de>
2016-05-14 12:55:05 +0200

    -erstellen von methodenköpfen

--- web/model/Cursor.dart
+++ web/model/Cursor.dart
@@ -1,18 +1,72 @@
 import "dart:math";
+import "Playfield.dart";
 
-enum direction
+enum Direction
 {
-up,
-right,
-down,
-left
+  up,
+  right,
+  down,
+  left
 }
 class Cursor
 {
   Point<int> _cursor1;
   Point<int> _cursor2;
-  Cursor(this._cursor1,this._cursor2)
-  {}
+  Playfield playfield;
+  Cursor(Point<int> cursor1, Point<int> cursor2, Playfield field)
+  {
+    this._cursor1 = cursor1;
+    this._cursor2 = cursor2;
+    this.playfield = field;
+  }
+
+  void move(Direction dir)
+  {
+    Point<int> up = new Point(_cursor1.x, _cursor1.y+1);
+    Point<int> right = new Point(_cursor2.x+1, _cursor2.y);
+    Point<int> down = new Point(_cursor1.x-1, _cursor1.y-1);
+    Point<int> left = new Point(_cursor1.x-1, _cursor1.y);
+
+    switch (dir) {
+
+      case Direction.up :
+        if(playfield.isValidCoords(up))
+        {
+          this._cursor1 = new Point(_cursor1.x, _cursor1.y+1);
+          this._cursor2 = new Point(_cursor2.x, _cursor2.y+1);
+        }
+        break;
+
+      case Direction.right :
+        if(playfield.isValidCoords(right))
+        {
+          this._cursor1 = new Point(_cursor1.x+1, _cursor1.y);
+          this._cursor2 = new Point(_cursor2.x+1, _cursor2.y);
+        }
+        break;
+
+      case Direction.down :
+        if(playfield.isValidCoords(down))
+        {
+          this._cursor1 = new Point(_cursor1.x, _cursor1.y-1);
+          this._cursor2 = new Point(_cursor2.x, _cursor2.y-1);
+        }
+        break;
+
+      case Direction.left :
+        if(playfield.isValidCoords(left))
+        {
+          this._cursor1 = new Point(_cursor1.x-1, _cursor1.y);
+          this._cursor2 = new Point(_cursor2.x-1, _cursor2.y);
+        }
+        break;
+    }
+
+  }
   void swap()
-  {}
+  {
+    Point<int> temp = new Point(_cursor1.x, _cursor1.y);
+    _cursor1 = new Point(_cursor2.x, _cursor2.y);
+    _cursor2 = new Point(temp.x, temp.y);
+  }
 }
--- web/model/Level.dart
+++ web/model/Level.dart
@@ -1,14 +1,47 @@
 import "Block.dart";
 class Level
 {
+  List<String> _colors;
+  List<String> _blocks;
+
+  List<List<Block>> _startField;
+  int _levelTimeSec;
+  int _comboHoldTime;
+  double _requiredScore;
+
   Level()
   {
-    List<String> _colors;
-    List<String> _blocks;
 
-    List<List<Block>> _startField;
-    int _levelTimeSec;
-    int _comboHoldTime;
-    double _requiredScore;
+  }
+
+
+  List<String> getColors ()
+  {
+    return _colors;
+  }
+
+  List<String> getBlocks()
+  {
+    return _blocks;
+  }
+
+  List<List<Block>> getStartField()
+  {
+    return _startField;
+  }
+
+  int getLevelTime()
+  {
+    return _levelTimeSec;
+  }
+
+  int getComboHoldTime()
+  {
+    return _comboHoldTime;
+  }
+
+  double getRequiredScore()
+  {
+    return _requiredScore;
   }
 }
